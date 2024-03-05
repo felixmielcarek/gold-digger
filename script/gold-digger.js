515e277bf353e2e544a9dd27345d4e73a6bda140
felixmielcarek <felix.mielcarek@etu.uca.fr>
2024-03-05 20:54:57 +0100

    :loud_sound: Add logs

--- script/gold-digger.js
+++ script/gold-digger.js
@@ -28,7 +28,7 @@ async function addPlaylistsToDictionary(dictionary, href = `https://api.spotify.
         response.data.items.forEach(element => dictionary[element.id] = element.name);
         if (response.data.next)
             await addPlaylistsToDictionary(dictionary, response.data.next)
-    } catch (error) { console.log(error.response.status) }
+    } catch (error) { webError("Add playlists to dictionnary", error) }
 }
 
 function GDPlaylistExists(playlistsDictionary) {
@@ -44,7 +44,7 @@ async function getUserId() {
     try {
         const response = await axios.get('https://api.spotify.com/v1/me', { headers: { 'Authorization': 'Bearer ' + accessToken, } });
         return response.data.id;
-    } catch (error) { console.log(error.response.status) }
+    } catch (error) { webError("Get user id", error) }
 }
 
 async function createPlaylist() {
@@ -65,7 +65,7 @@ async function createPlaylist() {
     try {
         const response = await axios(options);
         GDPlaylistId = response.data.id; //will be needed later
-    } catch (error) { console.log(error.response.status) }
+    } catch (error) { webError("Create playlist", error) }
 }
 
 async function createsGDPlaylist() {
@@ -88,7 +88,7 @@ async function addPlaylistsArtistsToList(href) {
         const response = await axios.get(href, { headers: { Authorization: 'Bearer ' + accessToken } });
         response.data.items.forEach(element => { element.track.artists.forEach(artist => { addArtistToList(artist.id) }) })
         if (response.data.next) await addPlaylistsArtistsToList(response.data.next);
-    } catch (error) { console.log(error.response.status) }
+    } catch (error) { webError("Add playlists artists to list", error) }
 }
 
 async function getPlaylistsArtists() {
@@ -101,7 +101,7 @@ async function getAlbumsArtists(href = `https://api.spotify.com/v1/me/albums?lim
         const response = await axios.get(href, { headers: { Authorization: 'Bearer ' + accessToken } })
         response.data.items.forEach(element => { element.album.tracks.items.forEach(track => track.artists.forEach(artist => addArtistToList(artist.id))) });
         if (response.data.next) await getAlbumsArtists(response.data.next)
-    } catch (error) { console.log(error.response.status) }
+    } catch (error) { webError("Get albums artists", error) }
 }
 
 async function getTracksArtists(href = `https://api.spotify.com/v1/me/tracks?limit=${spotifyRequestsLimit}&offset=0`) {
@@ -109,7 +109,7 @@ async function getTracksArtists(href = `https://api.spotify.com/v1/me/tracks?lim
         const response = await axios.get(href, { headers: { Authorization: 'Bearer ' + accessToken } });
         response.data.items.forEach(element => element.track.artists.forEach(artist => addArtistToList(artist.id)));
         if (response.data.next) await getTracksArtists(response.data.next)
-    } catch (error) { console.log(error.response.status) }
+    } catch (error) { webError("Get tracks artists", error) }
 }
 
 //#endregion
@@ -124,29 +124,60 @@ async function getArtistTracks(href) {
         const artistResponse = await axios.get(href, { headers: { Authorization: 'Bearer ' + accessToken } });
         for (const album of artistResponse.data.items) {
             if (album.release_date === currentDate) {
-                const albumResponse = await axios.get(`https://api.spotify.com/v1/albums/${album.id}/tracks?limit=${spotifyRequestsLimit}&offset=0`)
-                albumResponse.data.items.forEach(track => addTracksToList(track.id));
+                try {
+                    const albumResponse = await axios.get(`https://api.spotify.com/v1/albums/${album.id}/tracks?limit=${spotifyRequestsLimit}&offset=0`, { headers: { Authorization: 'Bearer ' + accessToken } })
+                    albumResponse.data.items.forEach(track => addTracksToList(track.id));
+                } catch (error) { webError("Get artist tracks", error) }
             }
         }
-    } catch (error) { console.log(error.response.status) }
+    } catch (error) { webError("Get artist albums", error) }
 }
 
 async function getTracks() {
     for (const artist of artistIds)
         await getArtistTracks(`https://api.spotify.com/v1/artists/${artist}/albums?limit=${spotifyRequestsLimit}&offset=0`);
-    console.log(trackIds);
+}
+//#endregion
+
+//#region LOGS
+function webError(step, error) {
+    console.log(`[KO] ${step} : ${error.response.data}`);
+}
+
+function stepSuccess(step) {
+    console.log(`[OK] ${step}`);
+}
+
+function stepBeggining(step) {
+    const sptor = "=".repeat(5);
+    console.log(`${sptor} ${step} ${sptor}`);
+}
+
+async function stepExecution(stepName, stepFunc) {
+    stepBeggining(stepName);
+    await stepFunc();
+    stepSuccess(stepName);
 }
 //#endregion
 
 //#region MAIN
 async function main() {
-    await createsGDPlaylist();
-
-    await getPlaylistsArtists();
-    await getAlbumsArtists();
-    await getTracksArtists();
-
-    await getTracks();
+    const step1 = "Playlist creation";
+    const step2 = "Playlists artists recovery";
+    const step3 = "Albums artists recovery";
+    const step4 = "Tracks artists recovery";
+    const step5 = "Artists tracks recovery";
+    const step6 = "Tracks addition";
+    try {
+        stepExecution(step1, createsGDPlaylist);
+        stepExecution(step2, getPlaylistsArtists);
+        stepExecution(step3, getAlbumsArtists);
+        stepExecution(step4, getTracksArtists);
+        stepExecution(step5, getTracks);
+        //stepExecution(step6);
+    } catch (error) {
+        console.log("An error occured");
+    }
 }
 //#endregion
 
